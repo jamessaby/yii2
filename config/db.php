1d15099b8fd78df77311a13f2e465efd744f36c2
James Saby <jamessaby@gmail.com>
2017-09-20 12:52:55 -0500

    commit all

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'sqlsrv:server=LAPTOP-ANCMMNLU;database=yii2basic',
+    'username' => 'yii2user',
+    'password' => 'yii2basic',
+    'charset' => 'utf8',
+];
---
+++ views/site/say.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: James
+ * Date: 9/15/2017
+ * Time: 3:47 PM
+ */
+use yii\helpers\Html;
+?>
+<?= Html::encode($message) ?>
